075c0d89b6ec667e9c81e9580c04f8127252a6a3
Zach Lewis <zachlewis@Zachs-MacBook-Pro.local>
2020-05-25 17:45:24 -0500

    Add FB Login Demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import './App.css';
 import FBLogin from './FBLogin';
-import SideHustle from './SideHustle';
+
 
 export function Privacy() {
   return (<div>ZALME LLC (MGM Side Hustle - 2020) - <a href="privacy.html">Privacy Policy</a></div>);
 }
 
-class App extends React.Component {
 
-  state = { isLoggedIn: false }
+
+class App extends React.Component {
 
   render() {
 
     return (
       <div className="App">
         <h1>MGM Side Hustle</h1>
-        {this.state.isLoggedIn ?  <SideHustle/> : <FBLogin/> }
+       
+        <FBLogin/> }
+
         <Privacy />
       </div>
     );
--- src/FBLogin.tsx
+++ src/FBLogin.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import FacebookLogin from 'react-facebook-login';
 import './App.css';
+import SideHustle from './SideHustle';
+
+
+interface State {
+  isLoggedIn: boolean
+}
 
 class FBLogin extends React.Component {
+ 
+  state = { isLoggedIn: false }
+
+  responseFacebook = (response: any) => {
+    console.log(response.status)
+    if (response.status === 'connected') {
+      this.setState({isLoggedIn: true})
+    } else {
+      this.setState({isLoggedIn: false})
+    }
+  }
 
   render() {
 
@@ -9,8 +27,15 @@ class FBLogin extends React.Component {
       <div className="App">
         <h1>MGM Side Hustle - Facebook Login</h1>
         <hr />
-        <button>Login With Facebook</button>
-        <hr/>    
+        {this.state.isLoggedIn ? <SideHustle/> :
+        <FacebookLogin
+          appId="253330662584482"
+          autoLoad={true}
+          fields="name,email,picture"
+          scope="public_profile,user_friends,user_actions.books"
+          callback={this.responseFacebook}
+        />}
+        <hr />
       </div>
     );
   }
